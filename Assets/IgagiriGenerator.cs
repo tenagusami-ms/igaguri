beb1742e020e74387929a5660cf27656903befa8
te nagusami <tenagusami@hotmail.com>
2022-03-21 15:52:56 +0900

    change: IDE test

--- Assets/IgagiriGenerator.cs
+++ Assets/IgagiriGenerator.cs
@@ -9,15 +9,13 @@ public class IgagiriGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var igaguri =
-                Instantiate(this.igaguriPrefab);
-            if (Camera.main is null) return;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            var worldDir = ray.direction;
-            igaguri.GetComponent<IgaguriController>().Shoot(
-                worldDir.normalized * 2000);
-        }
+        if (!Input.GetMouseButtonDown(0)) return;
+        var igaguri =
+            Instantiate(this.igaguriPrefab);
+        if (Camera.main is null) return;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var worldDir = ray.direction;
+        igaguri.GetComponent<IgaguriController>().Shoot(
+            worldDir.normalized * 2000);
     }
 }
